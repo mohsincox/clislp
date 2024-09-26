baee37ffa4bd82725ec6c021a992c85a7e3859e8
Mohsin <mohsincse2015@gmail.com>
2024-09-26 15:03:26 +0600

    ssp init configured

--- src/pages/admin/country/CountryList.jsx
+++ src/pages/admin/country/CountryList.jsx
@@ -5,7 +5,13 @@ import { toast } from "react-toastify";
 import { API_PUBLIC_URL } from "../../../constants";
 
 export default function CountryList() {
-  const [countryList, setCountryList] = useState([]);
+  const [countriesData, setCountriesData] = useState({
+    countries: [],
+    currentPage: 1,
+    pageSize: 5,
+    totalCountries: 0,
+    totalPages: 0,
+  });
   const navigate = useNavigate();
   const getLoginData = localStorage.getItem("loginData");
 
@@ -23,7 +29,7 @@ export default function CountryList() {
             },
           })
           .then((response) => {
-            setCountryList(response.data);
+            setCountriesData(response.data);
           })
           .catch((error) => {
             console.log(error);
@@ -62,7 +68,7 @@ export default function CountryList() {
                   },
                 })
                 .then((response) => {
-                  setCountryList(response.data);
+                  setCountriesData(response.data);
                 })
                 .catch((error) => {
                   console.log(error);
@@ -83,6 +89,50 @@ export default function CountryList() {
       })();
     }
   }
+
+  const handleNextPage = () => {
+    if (countriesData.currentPage < countriesData.totalPages) {
+      handlePagination(countriesData.currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (countriesData.currentPage > 1) {
+      handlePagination(countriesData.currentPage - 1);
+    }
+  };
+
+  const handlePagination = (currentPage) => {
+    const storageData = JSON.parse(getLoginData);
+    const token = storageData.accessToken;
+    (async () => {
+      await axios
+        .get(
+          `${API_PUBLIC_URL}api/countries?page=${currentPage}&limit=${countriesData.pageSize}`,
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        )
+        .then((response) => {
+          console.log("🚀 ~ .then ~ response.data:", response.data);
+          setCountriesData(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response.status === 403) {
+            toast.error("No Permission");
+            navigate("/admin/no-permission");
+          }
+        });
+    })();
+  };
+
+  const pageNumbers = Array.from(
+    { length: countriesData.totalPages },
+    (_, index) => index + 1
+  );
 
   return (
     <>
@@ -113,9 +163,13 @@ export default function CountryList() {
                 </tr>
               </thead>
               <tbody>
-                {countryList.map((country, index) => (
+                {countriesData?.countries.map((country, index) => (
                   <tr key={country.id}>
-                    <td>{index + 1}</td>
+                    <td>
+                      {countriesData.pageSize *
+                        (countriesData.currentPage - 1) +
+                        (index + 1)}
+                    </td>
                     <td>{country.name}</td>
                     <td>{country.short_name}</td>
                     <td>
@@ -148,6 +202,41 @@ export default function CountryList() {
                 ))}
               </tbody>
             </table>
+          </div>
+
+          <div className="d-flex justify-content-between align-items-center my-3">
+            <span className="badge bg-primary">
+              Page {countriesData.currentPage} of {countriesData.totalPages}
+            </span>
+            <div>
+              <button
+                className="btn btn-outline-primary me-2"
+                onClick={handlePreviousPage}
+                disabled={countriesData.currentPage === 1}
+              >
+                Previous
+              </button>
+              {pageNumbers.map((page) => (
+                <button
+                  key={page}
+                  className={`btn btn-outline-primary me-1 ${
+                    countriesData.currentPage === page ? "active" : ""
+                  }`}
+                  onClick={() => handlePagination(page)}
+                >
+                  {page}
+                </button>
+              ))}
+              <button
+                className="btn btn-outline-primary ms-2"
+                onClick={handleNextPage}
+                disabled={
+                  countriesData.currentPage === countriesData.totalPages
+                }
+              >
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
